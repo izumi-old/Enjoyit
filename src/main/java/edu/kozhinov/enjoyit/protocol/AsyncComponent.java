a61d92d1e382e1d5ea86cafdc1dec00e95235c66
Aiden <kozhinov.mi@gmail.com>
2021-05-18 08:48:56 +0400

    Refactoring Change AsyncComponent conception

--- src/main/java/edu/kozhinov/enjoyit/protocol/AsyncComponent.java
+++ src/main/java/edu/kozhinov/enjoyit/protocol/AsyncComponent.java
@@ -3,28 +3,11 @@ package edu.kozhinov.enjoyit.protocol;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public abstract class AsyncComponent {
-    private final Thread thread;
+public abstract class AsyncComponent extends Thread {
 
-    public AsyncComponent(Thread thread) {
-        this.thread = thread;
-    }
-
+    @Override
     public void start() {
-        if (!thread.isAlive()) {
-            thread.start();
-            log.debug("an async component <{}> was started", hashCode());
-        }
-    }
-
-    public boolean isAlive() {
-        return thread.isAlive();
-    }
-
-    public void stop() {
-        if (thread.isAlive()) {
-            thread.interrupt();
-            log.debug("an async component <{}> was stopped", hashCode());
-        }
+        log.debug("an async component <{}> was started", hashCode());
+        super.start();
     }
 }
--- src/main/java/edu/kozhinov/enjoyit/protocol/io/SocketAsyncListener.java
+++ src/main/java/edu/kozhinov/enjoyit/protocol/io/SocketAsyncListener.java
@@ -9,29 +9,23 @@ import java.util.concurrent.BlockingQueue;
 
 @Slf4j
 public class SocketAsyncListener extends AsyncComponent {
+    private final BlockingQueue<Data> data;
+    private final Reader reader;
+
     public SocketAsyncListener(BlockingQueue<Data> data, Reader reader) {
-        super(new Thread(new Listener(data, reader)));
+        this.data = data;
+        this.reader = reader;
         log.debug("a socket async listener <{}>, <{}> is ready to work", super.hashCode(), hashCode());
     }
 
-    private static final class Listener implements Runnable {
-        private final BlockingQueue<Data> data;
-        private final Reader reader;
-
-        Listener(BlockingQueue<Data> data, Reader reader) {
-            this.data = data;
-            this.reader = reader;
-        }
-
-        @Override
-        public void run() {
-            try {
-                while (!Thread.interrupted()) {
-                    data.add(reader.read());
-                }
-            } catch (IOException ex) {
-                log.error(ex.getMessage());
+    @Override
+    public void run() {
+        try {
+            while (!Thread.interrupted()) {
+                data.add(reader.read());
             }
+        } catch (IOException ex) {
+            log.error(ex.getMessage());
         }
     }
 }
